894894c7ae85e7767231ff1785ce3cbc66d28227
Chiara Pinto <pintochiara@itis-molinari.eu>
2024-12-16 10:38:02 +0100

    js corretto

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const hide = (elements) => {
-    elements.forEach((element) => {
-       element.classList.add("hidden");
-       element.classList.remove("visible");
-    }); 
-}
- 
-const show = (element) => {
-    element.classList.add("visible");
-    element.classList.remove("hidden");   
-}
- 
-export const createNavigator = (parentElement) => {
-    const pages = Array.from(parentElement.querySelectorAll(".page"));
+import { createNavigator } from "./navigator.js";
 
-    const render = () => {
-        const url = new URL(document.location.href);
-        const pageName = url.hash.replace("#", "");
-        const selected = pages.filter((page) => page.id === pageName)[0] || pages[0];
-
-        hide(pages);
-        show(selected);
-    }
-    window.addEventListener('popstate', render); 
-    render();   
-}
+const navigator = createNavigator(document.querySelector("#container"));
